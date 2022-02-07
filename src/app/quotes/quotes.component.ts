9db446e795fa6d33c2bde8e8f84bc4a395013799
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-07 10:43:43 +0300

    added index in the quote interpolation loop

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -12,6 +12,7 @@ export class QuotesComponent implements OnInit {
   quote : Quote[] =[
     new Quote(1,'Every market has a mad man','Wahenga','Elijah',new Date(2,2,2012) ),
     new Quote(2, 'sharpen your axe','me','kiragu',new Date (3,4,2022)), 
+    new Quote (3,'hahahahahah','eliki','eliki', new Date (2,3,2016))
   ]
 
   constructor() { }
